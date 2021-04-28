b1e28a05d289da8b7aec394206caf81299272cb9
Mario Norberto Garcia <mario@inft.com.ar>
2021-04-28 15:35:17 -0300

    Tutorial 35

--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Loading from '../../components/Loading'
 
 export default function UserLogged() {
     return (
         <View>
-            <Text>UserLogged....</Text>
+            <Text>UserGuest...</Text>
+            <Loading
+                isVisble={true}
+                text="Cargando..."
+            />
         </View>
     )
 }
